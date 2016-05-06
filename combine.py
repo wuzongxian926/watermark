882d456731d3a098b4aed22631306c34b1e98440
daleloogn <daleloogn@126.com>
2016-05-06 12:55:14 +0800

    daleloogn

---
+++ combine.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import os
+from Tkinter import *
+
+reload(sys)
+sys.setdefaultencoding('gbk')
+flag = True
+print u"你好!"
+
+
+def file_path(FILE_PATH='/home/wuxy/aaa111/22222'):
+    if os.path.isdir(FILE_PATH):  ##不用加引号，如果是多级目录，只判断最后一级目录是否存在
+        print 'dir %s exists' % (FILE_PATH)
+        pass
+    else:
+        print  'dir %s not exists' % (FILE_PATH)
+        os.mkdir(FILE_PATH)  ##只能创建单级目录，用这个命令创建级联的会报OSError错误
+        print 'mkdir ok'
+    return 0
+
+
+file_path('combine')
+file_path('temp')
+file_path('out')
+
+
+class Application(Frame):
+    def get_input_title(self, path):
+        file_name = os.path.basename(path)
+        return file_name
+
+    def get_input_format(self, path):
+        format = os.path.basename(path).split('.')[-1]
+        return format
+
+    def combine_video(self, dir="combine"):
+        video1 = ''
+        video2 = ''
+        for parent, dirname, filename in os.walk(dir):
+            for file in filename:
+                print file
+                path = os.path.join(parent, file)
+                if video1 == '':
+                    video1 = path
+                else:
+                    video2 = path
+        input_title1 = self.get_input_title(video1)
+        input_title2 = self.get_input_title(video2)
+        input_format=self.get_input_format(video1)
+        output = input_title1 + input_title2
+        output_format=input_format
+        cmd_line = "ffmpeg -i " + video1 + " -vcodec copy -acodec copy -vbsf h264_mp4toannexb temp/" + output + "_1.ts"
+        os.system(cmd_line)
+        cmd_line = "ffmpeg -i " + video2 + " -vcodec copy -acodec copy -vbsf h264_mp4toannexb temp/" + output + "_2.ts"
+        os.system(cmd_line)
+        cmd_line = "ffmpeg -i \"concat:temp/" + output + "_1.ts|temp/" + output + "_2.ts\"  -vcodec copy -acodec copy -absf aac_adtstoasc out/" + output + "."+output_format
+        print(cmd_line)
+        os.system(cmd_line)
+        os.remove("temp/" + output + "_1.ts")
+        os.remove("temp/" + output + "_2.ts")
+        print(u'合并完毕前往out查看')
+        return 0
+
+    def createWidgets(self):
+        self.ssycom = Button(self)
+        self.ssycom["text"] = "合并视频",
+        self.ssycom["command"] = self.combine_video
+        self.ssycom.pack({"side": "left"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+
+root = Tk()
+w = Label(root, text="watermarking")
+w.pack()
+app = Application(master=root)
+doc = Label(root, text="需合并的两个文件放置于combine文件夹", fg="red")
+doc.pack()
+app.mainloop()
---
+++ split.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import os
+from Tkinter import *
+
+reload(sys)
+sys.setdefaultencoding('gbk')
+flag = True
+print u"你好!"
+
+
+def file_path(FILE_PATH='/home/wuxy/aaa111/22222'):
+    if os.path.isdir(FILE_PATH):  ##不用加引号，如果是多级目录，只判断最后一级目录是否存在
+        print 'dir %s exists' % (FILE_PATH)
+        pass
+    else:
+        print  'dir %s not exists' % (FILE_PATH)
+        os.mkdir(FILE_PATH)  ##只能创建单级目录，用这个命令创建级联的会报OSError错误         print 'mkdir ok
+    return 0
+
+
+file_path('split')
+file_path('out')
+
+
+class Application(Frame):
+    def get_input_title(self, path):
+        file_name = os.path.basename(path)
+        return file_name
+
+    def get_input_format(self, path):
+        format = os.path.basename(path).split('.')[-1]
+        return format
+
+    def split_video_a(self, input_path='split/video.mp4', end="00:45:00"):
+        start = '00:00:00'
+        input_title = self.get_input_title(input_path)
+        output_title = input_title
+        output_format = self.get_input_format(input_path)
+        cmd_line = "ffmpeg -ss " + start + " -t " + end + " -i " + input_path + \
+                   " -vcodec copy -acodec copy -y " + "out/" + output_title + u"_上集." + output_format
+        print(cmd_line)
+        os.system(cmd_line)
+        return 0
+
+    def split_video_b(self, input_path='split/video.mp4', start='00:45:00'):
+        input_title = self.get_input_title(input_path)
+        output_title = input_title
+        output_format = self.get_input_format(input_path)
+        cmd_line = "ffmpeg -ss " + start + "  -i " + input_path + " -vcodec copy -acodec copy -y " \
+                   + "out/" + output_title + u"_下集." + output_format
+        print(cmd_line)
+        os.system(cmd_line)
+        return 0
+
+    def split_video(self, input_dir='split', split_time="00:45:00"):  # 仅需修改此处分割时间即可 *****************
+        for parent, dirname, filename in os.walk(input_dir):
+            for file in filename:
+                print file
+                path = os.path.join(parent, file)
+                self.split_video_a(path, split_time)
+                self.split_video_b(path, split_time)
+        print u"视频分割完毕，到out文件夹查看"
+        return 0
+
+    def createWidgets(self):
+        self.ssyspl = Button(self)
+        self.ssyspl["text"] = u"分割视频",
+        self.ssyspl["command"] = self.split_video
+        self.ssyspl.pack({"side": "left"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+
+root = Tk()
+w = Label(root, text="watermarking")
+w.pack()
+app = Application(master=root)
+doc = Label(root, text="视频放置于split文件夹 ", fg="red")
+doc.pack()
+app.mainloop()
